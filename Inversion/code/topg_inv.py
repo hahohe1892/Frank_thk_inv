073be462e63b83b5ca9468e88868d23d6dce1d52
Thomas Frank <thomas.frank@geo.uu.se>
2026-03-03 09:57:15 +0100

    small change to copyright in topg_inv

--- Inversion/code/topg_inv.py
+++ Inversion/code/topg_inv.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2021-2023 Guillaume Jouvet <guillaume.jouvet@unil.ch>
 # Published under the GNU GPL (Version 3), check at the LICENSE file
 
 import datetime, time
